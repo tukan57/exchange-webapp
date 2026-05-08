7d7e97b324f7739315a9f071789a067c0f14386f
tukan57 <tukan57@seznam.cz>
2026-05-08 15:17:30 +0200

    Feature/Hosting - RENDER setup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from config import Config
 from routes.auth import auth_bp
 from routes.api import api_bp
@@ -17,6 +18,9 @@ def create_app():
     
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
